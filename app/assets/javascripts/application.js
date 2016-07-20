bdfbe565325ae1e52376da7a9436778f811b20cc
Danny Pham <email": null>
2016-07-20 04:49:18 -0700

    homepage slidey pictures and click it to show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,42 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var y = document.getElementsByClassName("small-slide");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+     y[i].style.opacity = "0.75";
+  }
+  x[slideIndex-1].style.display = "block";
+  y[slideIndex-1].style.opacity = "1";
+
+  // carousel(slideIndex-1);
+}
+
+// function carousel(index) {
+//     var i;
+//     var x = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < x.length; i++) {
+//       x[i].style.display = "none";
+//     }
+//     index++;
+//     if (index > x.length) {index = 1}
+//     x[index-1].style.display = "block";
+//     setTimeout(carousel, 2000); // Change image every 2 seconds
+// }
