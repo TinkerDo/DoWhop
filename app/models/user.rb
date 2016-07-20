19281be3a98e0c675a5313952b3daaa046b90ae6
Danny Pham <email": null>
2016-07-20 06:05:50 -0700

    add event and attendance association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :attendances, :foreign_key => "attendee_id", :dependent => :destroy
+  has_many :attending, :through => :attendances, :source => :event
+  has_many :hosted, class_name: "Event", foreign_key: "host_id"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Event.delete_all
+Attendance.delete_all
+
+user = User.create(first_name: 'danny', last_name: 'pham', email: 'do@whop.com', password: 'password')
+host = User.create(first_name: 'rae', last_name: 'lietzau', email: 'rae.lietzau@dowhop.com', password: 'password')
+
+event1 = Event.create(name: 'surfing', cost: '$100', description: 'you surf and turf and etc.', location: 'pb', host_id: host.id)
+event2 = Event.create(name: 'painting', cost: '$50', description: 'take a paintbrush dip in paint and brush', location: 'downtown', host_id: host.id)
+event3 = Event.create(name: 'jumproping', cost: '$1', description: 'take a rope and jump over it', location: 'north park', host_id: host.id)
+event4 = Event.create(name: 'programming', cost: '$30', description: 'learn to code in languages', location: 'south park', host_id: host.id)
+
+Attendance.create(attendee_id: user.id, event_id: event1.id)
+Attendance.create(attendee_id: user.id, event_id: event2.id)
+Attendance.create(attendee_id: user.id, event_id: event3.id)
+Attendance.create(attendee_id: user.id, event_id: event4.id)
